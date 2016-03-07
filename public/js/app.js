edb3fa3adb1fcaf2a259844326e64239537f46ab
Matth96 <Matthias.Dolstra@hva.nl>
2016-03-07 15:22:56 +0100

    delete jQuery

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,43 @@
-$(document).ready(function(){
-    $('.product').first().addClass('product-active');
-    $(".product-indicator[data-uuid='"+$('.product').first().data('uuid')+"']").addClass('product-indicator-active');
+var helper = (function () {
+    function one(selector) {
+        return document.querySelector(selector);
+    };
 
-    $('.product-indicator').on('click', function(e){
-        var id = $(e.currentTarget).data('uuid');
-        $('.product-indicator-active').removeClass('product-indicator-active');
-        $(e.currentTarget).addClass('product-indicator-active');
-        $('.product.product-active').removeClass('product-active');
-        $(".product[data-uuid='"+id+"']").addClass('product-active');
-    });
-});
+    function all(selector) {
+        return document.querySelectorAll(selector);
+    };
+
+    function addClass(element, className) {
+        one(element).classList.add(className);
+    }
+
+    function removeClass(element, className) {
+        one(element).classList.remove(className);
+    }
+    return {
+        one: one,
+        all: all,
+        addClass: addClass,
+        removeClass: removeClass
+    };
+})();
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var uuid = helper.one('.product').attributes['data-uuid'].value,
+        productIndicator = helper.all('.product-indicator');
+
+    helper.addClass('.product', 'product-active');
+    helper.addClass('.product-indicator[data-uuid=\'' + uuid + "']", 'product-indicator-active');
+
+    for (var p = 0; p < productIndicator.length; p++) {
+        productIndicator[p].addEventListener('click', function (e) {
+            var id = e.currentTarget.attributes[2].nodeValue;
+            helper.removeClass('.product-indicator-active', 'product-indicator-active');
+            e.currentTarget.classList.add('product-indicator-active');
+
+            helper.removeClass('.product.product-active', 'product-active');
+            helper.addClass(".product[data-uuid='" + id + "']", 'product-active');
+        });
+    }
+}, false);
