8a67cab92daceab1764a9a07d86d1a4727fe8c6d
Matth96 <Matthias.Dolstra@hva.nl>
2016-03-08 09:57:21 +0100

    combine vars

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,41 @@
-var express = require('express');
-var router = express.Router();
-var fs = require('fs');
+var express = require('express'),
+    router = express.Router(),
+    fs = require('fs');
 
-router.get('/', function(req, res, next) {
-    fs.readFile('resources/feed.json', 'utf8', function(err, data) {
-        if(err) {
+router.get('/', function (req, res, next) {
+    fs.readFile('resources/feed.json', 'utf8', function (err, data) {
+        if (err) {
             res.status(404);
             next();
         }
 
-        res.render('feed', { title: 'Feed', items: JSON.parse(data) });
+        res.render('feed', {
+            title: 'Feed',
+            items: JSON.parse(data)
+        });
     })
 });
 
-router.get('/appearance/:uuid', function(req, res, next) {
-    fs.readFile('resources/appearance/'+req.params.uuid+'.json', 'utf8', function(err, data) {
-        if(err) {
+router.get('/appearance/:uuid', function (req, res, next) {
+    fs.readFile('resources/appearance/' + req.params.uuid + '.json', 'utf8', function (err, data) {
+        if (err) {
             res.status(404);
             next();
         }
 
-        var item = JSON.parse(data);
-        var products = [];
+        var item = JSON.parse(data),
+            products = [];
 
-        item.product_occurrences.forEach(function(occurrence) {
-            var product = fs.readFileSync('resources/product/'+occurrence.product.id+'.json', 'utf8');
+        item.product_occurrences.forEach(function (occurrence) {
+            var product = fs.readFileSync('resources/product/' + occurrence.product.id + '.json', 'utf8');
             products.push(JSON.parse(product));
         });
 
-        console.log(products);
-
-        res.render('appearance', { title: item.title, item: item , products: products});
+        res.render('appearance', {
+            title: item.title,
+            item: item,
+            products: products
+        });
     })
 });
 
