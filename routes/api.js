db401e31409118e36819c5dc21a1ea8d8ceb98e1
Matth96 <Matthias.Dolstra@hva.nl>
2016-03-09 10:55:16 +0100

    update api with rendering html server side

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,46 @@ var fs = require('fs'),
     router = express.Router();
 
 router.get('/feed', function (req, res, next) {
-    sendFile('feed.json', res);
+    //    sendFile('feed.json', res);
+
+    fs.readFile('resources/feed.json', 'utf8', function (err, data) {
+        if (err) {
+            res.status(404);
+            next();
+        }
+
+        res.render('feed', {
+            title: 'Feed',
+            items: JSON.parse(data),
+            layout: false
+        });
+
+    })
 });
 
 router.get('/appearance/:uuid', function (req, res, next) {
-    sendFile('appearance/' + req.params.uuid + '.json', res);
+    //    sendFile('appearance/' + req.params.uuid + '.json', res);
+    fs.readFile('resources/appearance/' + req.params.uuid + '.json', 'utf8', function (err, data) {
+        if (err) {
+            res.status(404);
+            next();
+        }
+
+        var item = JSON.parse(data),
+            products = [];
+
+        item.product_occurrences.forEach(function (occurrence) {
+            var product = fs.readFileSync('resources/product/' + occurrence.product.id + '.json', 'utf8');
+            products.push(JSON.parse(product));
+        });
+
+        res.render('appearance', {
+            title: item.title,
+            item: item,
+            products: products,
+            layout: false
+        });
+    })
 });
 
 router.get('/product/:uuid', function (req, res, next) {
