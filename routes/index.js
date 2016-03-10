ab5d03acec6ba13482468be12b97158520011de0
Fons Hettema <fons.hettema@hva.nl>
2016-03-10 20:52:05 +0100

    Render home page via router

--- routes/index.js
+++ routes/index.js
@@ -8,25 +8,4 @@ router.get('/', function(req, res, next) {
     });
 });
 
-// router.get('/appearance/:uuid', function(req, res, next) {
-//     fs.readFile('resources/appearance/'+req.params.uuid+'.json', 'utf8', function(err, data) {
-//         if(err) {
-//             res.status(404);
-//             next();
-//         }
-
-//         var item = JSON.parse(data);
-//         var products = [];
-
-//         item.product_occurrences.forEach(function(occurrence) {
-//             var product = fs.readFileSync('resources/product/'+occurrence.product.id+'.json', 'utf8');
-//             products.push(JSON.parse(product));
-//         });
-
-//         console.log(products);
-
-//         res.render('appearance', { title: item.title, item: item , products: products});
-//     })
-// });
-
 module.exports = router;
