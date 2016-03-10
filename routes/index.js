7615a37f4a3d1f346c9c970c79b26244d549245c
Fons Hettema <fons.hettema@hva.nl>
2016-03-10 14:32:43 +0100

    Render main

--- routes/index.js
+++ routes/index.js
@@ -3,35 +3,30 @@ var router = express.Router();
 var fs = require('fs');
 
 router.get('/', function(req, res, next) {
-    fs.readFile('resources/feed.json', 'utf8', function(err, data) {
-        if(err) {
-            res.status(404);
-            next();
-        }
-
-        res.render('feed', { title: 'Feed', items: JSON.parse(data) });
-    })
+    res.render('main', {
+        title: 'feed'
+    });
 });
 
-router.get('/appearance/:uuid', function(req, res, next) {
-    fs.readFile('resources/appearance/'+req.params.uuid+'.json', 'utf8', function(err, data) {
-        if(err) {
-            res.status(404);
-            next();
-        }
+// router.get('/appearance/:uuid', function(req, res, next) {
+//     fs.readFile('resources/appearance/'+req.params.uuid+'.json', 'utf8', function(err, data) {
+//         if(err) {
+//             res.status(404);
+//             next();
+//         }
 
-        var item = JSON.parse(data);
-        var products = [];
+//         var item = JSON.parse(data);
+//         var products = [];
 
-        item.product_occurrences.forEach(function(occurrence) {
-            var product = fs.readFileSync('resources/product/'+occurrence.product.id+'.json', 'utf8');
-            products.push(JSON.parse(product));
-        });
+//         item.product_occurrences.forEach(function(occurrence) {
+//             var product = fs.readFileSync('resources/product/'+occurrence.product.id+'.json', 'utf8');
+//             products.push(JSON.parse(product));
+//         });
 
-        console.log(products);
+//         console.log(products);
 
-        res.render('appearance', { title: item.title, item: item , products: products});
-    })
-});
+//         res.render('appearance', { title: item.title, item: item , products: products});
+//     })
+// });
 
 module.exports = router;
